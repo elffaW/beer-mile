02d0b1a406349ab225cfdb651accecb215972722
elffaW <elffaW@users.noreply.github.com>
2017-05-18 18:11:03 -0400

    listen for buttons after data loads

--- index.js
+++ index.js
@@ -12,33 +12,36 @@ db.dbConnection.sync({force:false}).
 		crud.loadData().
 			then(function(done) {
 				console.log('Data loaded');
+
+
+				var DASH_MAC_ADDRESSES = [];	//get list of button MACs from database (pre-populated)
+
+				//REMOVE FOLLOWING LINE AFTER TESTING
+				DASH_MAC_ADDRESSES.push("68:54:fd:27:a0:ba"); //temp (MAC of poof button)
+
+				//get all the button MAC addresses
+				crud.getAllButtonMACs().
+					then(function(allMACs) {
+						console.log(`Got button list from DB [${allMACs}]`);
+						DASH_MAC_ADDRESSES = allMACs;
+					}).catch(function(err) {
+						console.error(err);
+					});
+
+				var dash = dash_button(DASH_MAC_ADDRESSES);
+
+				console.log(`Listening for presses from these MACs: [${DASH_MAC_ADDRESSES}]`);
+				//listen for button presses from any of the MACs in the DB
+				dash.on("detected", function (dash_mac){
+				    //insert a new timestamp into the DB
+				    console.log(`Detected button with MAC [${dash_mac}]`);
+				    console.log(`Inserting timestamp into DB [${new Date()}]`);
+				    crud.insertTimestamp(dash_mac, new Date());
+				});
+				
 			}).
 			catch(function(err) {
 				console.errog(`Data load error: ${err}`);
 			});
 	});
 
-var DASH_MAC_ADDRESSES = [];	//get list of button MACs from database (pre-populated)
-
-//REMOVE FOLLOWING LINE AFTER TESTING
-DASH_MAC_ADDRESSES.push("68:54:fd:27:a0:ba"); //temp (MAC of poof button)
-
-//get all the button MAC addresses
-crud.getAllButtonMACs().
-	then(function(allMACs) {
-		console.log(`Got button list from DB [${allMACs}]`);
-		DASH_MAC_ADDRESSES = allMACs;
-	}).catch(function(err) {
-		console.error(err);
-	});
-
-var dash = dash_button(DASH_MAC_ADDRESSES);
-
-console.log(`Listening for presses from these MACs: [${DASH_MAC_ADDRESSES}]`);
-//listen for button presses from any of the MACs in the DB
-dash.on("detected", function (dash_mac){
-    //insert a new timestamp into the DB
-    console.log(`Detected button with MAC [${dash_mac}]`);
-    console.log(`Inserting timestamp into DB [${new Date()}]`);
-    crud.insertTimestamp(dash_mac, new Date());
-});

4b9fd9adcbd9bc8f73ddfdd9b75c37a754c89e03
elffaW <elffaW@users.noreply.github.com>
2017-05-18 17:27:06 -0400

    fix crud function return values

--- db/crud.js
+++ db/crud.js
@@ -7,40 +7,44 @@ var db = require('./db');
 //returns array of all button MAC addresses, or sends rejection on error or if there are no buttons in the DB 
 module.exports = {
 	getAllButtonMACs: function() {
-		db.button.findAll().	//get all the buttons
-			then(function(buttons) {
-				var macs = [];
-				buttons.forEach(function(b) {	//put all the MACs into an array
-					macs.push(b.mac);
-				});
+		return new Promise(function(resolve, reject) {
+			db.button.findAll().	//get all the buttons
+				then(function(buttons) {
+					var macs = [];
+					buttons.forEach(function(b) {	//put all the MACs into an array
+						macs.push(b.mac);
+					});
 
-				if(macs.length > 1) {
-					resolve(macs);	//return the array
-				} else {
-					reject("No buttons found!");	//no buttons!
-				}
-			}).catch(function(err) {
-				reject(err);	//return the error
-			});
+					if(macs.length > 1) {
+						resolve(macs);	//return the array
+					} else {
+						reject("No buttons found!");	//no buttons!
+					}
+				}).catch(function(err) {
+					reject(err);	//return the error
+				});
+		});
 	},
 
 
 //takes a button MAC address, and inserts a new timestamp for that button
 	insertTimestamp: function(mac, timestamp) {
-		Button.findOne({	//find the button that was pressed
-			where: { mac: mac }
-		}).
-		then(function(button) {
-			var newTime = {};
-			newTime.timestamp = timestamp;
-			Timelog.create(newTime).	//create a new button, and...
-			then(function(timelog) {
-				timelog.setButton(button);	//link it to the button
+		return new Promise(function(resolve, reject) {
+			db.button.findOne({	//find the button that was pressed
+				where: { mac: mac }
+			}).
+			then(function(button) {
+				var newTime = {};
+				newTime.timestamp = timestamp;
+				Timelog.create(newTime).	//create a new button, and...
+				then(function(timelog) {
+					timelog.setButton(button);	//link it to the button
+				}).catch(function(err) {
+					reject(err);
+				});
 			}).catch(function(err) {
 				reject(err);
 			});
-		}).catch(function(err) {
-			reject(err);
 		});
 	}
 };

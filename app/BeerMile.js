9863a2b40177320dd078acce94ef69cd0395a6f7
elffaW <elffaW@users.noreply.github.com>
2017-07-07 18:00:22 -0400

    add stylesheet

--- app/BeerMile.js
+++ app/BeerMile.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 const io = require('socket.io-client');
 const socket = io();
 
+// import style from './styles/style.css';
+require('css!./styles/style.css');
+
 export class BeerMile extends React.Component {
 	constructor() {
 		super();

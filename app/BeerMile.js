105aa559ab2b6be41d3a4db71a493e772b30f71c
elffaW <elffaW@users.noreply.github.com>
2017-07-09 21:34:05 -0400

    add material-ui + deps (in progress)

--- app/BeerMile.js
+++ app/BeerMile.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MuiThemeProvider } from 'material-ui/styles';
+
+// import UI elements
+import Button from 'material-ui/Button';
 
 const io = require('socket.io-client');
 const socket = io();
 
 // import style from './styles/style.css';
-require('css!./styles/style.css');
+import './styles/style.css';
+// import './styles/fonts';
+// import 'grommet/scss/vanilla/index';
 
 export class BeerMile extends React.Component {
 	constructor() {
@@ -21,9 +27,11 @@ export class BeerMile extends React.Component {
 	render() {
 		console.log(this.state.socketMessage);
 		return (
-			<div>
-				<h2>TC LOSES</h2>
-			</div>
+			<MuiThemeProvider>
+				<Button>
+					<i class="material-icons">&#xE87C;</i>TC LOSES
+				</Button>
+			</MuiThemeProvider>
 		);
 	}
 }

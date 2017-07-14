93904a43b4ac7fc3b2d00b361d9c8d75754db871
elffaW <elffaW@users.noreply.github.com>
2017-07-14 18:20:16 -0400

    uncomment dash listener

--- app/server.js
+++ app/server.js
@@ -70,40 +70,40 @@ app.get('/', function(req, res) {
 });
 
 /************************ LISTEN FOR BUTTON PRESSES ************************/
-// db.dbConnection.sync({force:false}).
-// 	then(function() {
-// 		console.log('Database created');
+db.dbConnection.sync({force:false}).
+	then(function() {
+		console.log('Database created');
 	
-// 		var DASH_MAC_ADDRESSES = [];	//get list of button MACs from database (pre-populated)
+		var DASH_MAC_ADDRESSES = [];	//get list of button MACs from database (pre-populated)
 
-// 		//get all the button MAC addresses
-// 		crud.getAllButtonMACs().
-// 			then(function(allMACs) {
-// 				// console.log(`Got button list from DB [${allMACs}]`);
-// 				DASH_MAC_ADDRESSES = allMACs;
+		//get all the button MAC addresses
+		crud.getAllButtonMACs().
+			then(function(allMACs) {
+				// console.log(`Got button list from DB [${allMACs}]`);
+				DASH_MAC_ADDRESSES = allMACs;
 
-// 				//below included for testing purposes
-// 				if(DASH_MAC_ADDRESSES.length < 1) {
-// 					DASH_MAC_ADDRESSES.push("68:54:fd:27:a0:ba"); // (MAC of poof button)
-// 				}
+				//below included for testing purposes
+				if(DASH_MAC_ADDRESSES.length < 1) {
+					DASH_MAC_ADDRESSES.push("68:54:fd:27:a0:ba"); // (MAC of poof button)
+				}
 
-// 				var dash = dash_button(DASH_MAC_ADDRESSES, null, null, 'all');
+				var dash = dash_button(DASH_MAC_ADDRESSES, null, null, 'all');
 
-// 				console.log(`Listening for presses from these MACs: [${DASH_MAC_ADDRESSES}]`);
-// 				console.log();
-// 				//listen for button presses from any of the MACs in the DB
-// 				dash.on("detected", function (dash_mac){
-// 				    //insert a new timestamp into the DB
-// 				    console.log(`Detected button with MAC [${dash_mac}], inserting timestamp into DB [${new Date()}]`);
-// 				    crud.insertTimestamp(dash_mac, new Date());
-//					//TODO add socket emit to notify frontend to update (event "runnerCheckpoint")
+				console.log(`Listening for presses from these MACs: [${DASH_MAC_ADDRESSES}]`);
+				console.log();
+				//listen for button presses from any of the MACs in the DB
+				dash.on("detected", function (dash_mac){
+				    //insert a new timestamp into the DB
+				    console.log(`Detected button with MAC [${dash_mac}], inserting timestamp into DB [${new Date()}]`);
+				    crud.insertTimestamp(dash_mac, new Date());
+					//TODO add socket emit to notify frontend to update (event "runnerCheckpoint")
 
-// 				});
+				});
 
-// 			}).catch(function(err) {
-// 				console.error(err);
-// 			});
+			}).catch(function(err) {
+				console.error(err);
+			});
 
 				
-// 	});
+	});
 
